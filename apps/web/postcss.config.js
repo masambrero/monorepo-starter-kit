8a82c12ccfc11ecd2b78c711d9794260e9bd9c01
msm <msm@finch-melrose.com>
2024-09-23 14:08:34 +0300

    update postcss config

--- apps/web/postcss.config.js
+++ apps/web/postcss.config.js
@@ -1,3 +1,20 @@
+const path = require('path');
+
 module.exports = {
-  plugins: {},
+  plugins: [
+    'postcss-flexbugs-fixes',
+    [
+      'postcss-preset-env',
+      {
+        autoprefixer: {
+          flexbox: 'no-2009',
+        },
+        stage: 3,
+        features: {
+          'custom-properties': false,
+          'nesting-rules': true,
+        },
+      },
+    ],
+  ],
 };
